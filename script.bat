264f3e634fbd2b4db8123c9e5a9f5b3504c36ee6
Marcin Wolski <64419373+martin-desktops@users.noreply.github.com>
2025-05-21 21:29:28 +0200

    Make template path relative

--- script.bat
+++ script.bat
@@ -1,9 +1,14 @@
 @echo off
-set SOURCE_CFG=%USERPROFILE%\Desktop\cs-config\autoexec.cfg
-set TARGET_CFG="C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\autoexec.cfg"
-set TARGET_DIR="C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg"
+setlocal
 
-copy /Y "%SOURCE_CFG%" %TARGET_CFG%
+set SCRIPT_DIR=%~dp0
+set SOURCE_CFG=%SCRIPT_DIR%autoexec.cfg
+set TARGET_DIR=C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg
+set TARGET_CFG=%TARGET_DIR%\autoexec.cfg
+
+copy /Y "%SOURCE_CFG%" "%TARGET_CFG%"
 
 REM Open destination folder in Explorer
-explorer %TARGET_DIR%
+explorer "%TARGET_DIR%"
+
+endlocal
